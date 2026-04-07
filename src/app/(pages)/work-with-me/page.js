3f89ef1120eac60f2345c4098a20a7848c057a54
Jessica <jessica.antoinette@outlook.com>
2026-04-07 13:11:19 +0100

    more formatting

--- src/app/(pages)/work-with-me/page.js
+++ src/app/(pages)/work-with-me/page.js
@@ -3,16 +3,16 @@ import connie from "../../../../public/assets/connie.webp"
 
 export default function WorkWithMe () {
     return (
-        <div className="flex flex-col justify-center p-10 gap-10 mt-30 items-center text-center">
+        <div className="flex flex-col justify-center p-10 gap-10 items-center text-center">
             <h2>WORK WITH ME</h2>
             <p>
 There are a number of ways to engage with this work, depending on where you are on your path and what kind of support you are seeking. All work is offered within a grounded, ethical, and trauma-aware container, with a strong emphasis on self-responsibility, integration, and inner union.
 I aim to support you on the journey back to your true self, helping you heal core wounds, integrate fragmented parts, clear and strengthen your energy system, and move toward greater inner alignment and sovereignty.
             </p>
 
-            <p className="text-bold text-shadow-md">Spot the difference 👀</p>
-
 <div className="flex gap-10 flex-col">
+<p className="text-semibold text-shadow-sm"><strong>Spot the difference 👀</strong></p>
+<div className="flex-row flex gap-10">
 <Image 
     src={connie}
     alt="bear kemode"
@@ -28,6 +28,7 @@ I aim to support you on the journey back to your true self, helping you heal cor
         height={200}
         className="object-cover rounded-full"
     />
+</div>
 </div>
 
         </div>
--- src/components/PackageCard.jsx
+++ src/components/PackageCard.jsx
@@ -2,19 +2,20 @@ import Image from "next/image";
 
 export default function PackageCard({ title, description, img_url }) {
   return (
-    <div className="rounded-2xl overflow-hidden shadow-md hover:shadow-xl hover:scale-[1.01] transition cursor-pointer bg-white w-70 h-150">
+    <div className="rounded-2xl overflow-hidden shadow-md hover:shadow-xl hover:scale-[1.01] transition cursor-pointer bg-white w-70">
       
+      <div className="relative h-44">
       <Image
         src={img_url}
         alt={title}
-        width={280}
-        height={100}
-        className="object-contain"
+        fill
+        className="object-cover"
         
       />
+      </div>
 
-      <div className="p-5 grid top-0">
-        <h5 className="font-semibold">{title}</h5>
+      <div className="p-5">
+        <h5 className="font-semibold text-sm min-h-15 leading-6 pb-5">{title}</h5>
         <p className="text-sm text-gray-600 line-clamp-2">
           {description}
         </p>
