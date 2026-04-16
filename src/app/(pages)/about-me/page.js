27fa4e5f4d1acd2ea0ac9d6185cc2ebb455b45c3
Jessica <jessica.antoinette@outlook.com>
2026-04-16 01:56:59 +0100

    work with me / about

--- src/app/(pages)/about-me/page.js
+++ src/app/(pages)/about-me/page.js
@@ -1,14 +1,53 @@
 import Image from "next/image"
+import connie from "../../../../public/assets/connie.webp"
+
 export default function AboutMe () {
     return (
         
-        <div>
+<div className="flex flex-col justify-center relative w-screen">
+
+  <div className="flex gap-5 flex-col p-10 items-center">
       <Image 
-        src="https://dinoanimals.com/wp-content/uploads/2023/11/Kermode-bear-10.jpg"
-        alt="bear kemode"
-        width={200}
-        height={200}
+      src={connie}
+      alt=""
+      width={700}
+      height={300}
+      className="flex bottom-10 p-4 rounded-3xl"
       />
-        </div>
-    )
+
+      <span className="font-calligraphy text-[50px]"> my journey</span>
+
+<div className="text-center gap-5 flex flex-col px-8">
+      <p>Hi, thank you for being here. What a journey this Twin Flame path is! When I first had soul recognition and started looking into Twin Flames, I remember thinking “Okay, I’ll just do my inner work and then I’ll get my twin back.”</p>
+
+      <p>Hahaha. How wrong I was.</p>
+
+      <p>So much has changed since then.</p>
+
+      <p>I’ve been to the depths of my own soul, through relentless inner work, energy clearing, and facing the mirror of my twin again and again. I’ve had days crying on the floor of my summer house, feeling like I was breaking into pieces and wondering if the pain would ever end</p> 
+      
+      <p>I’ve also had moments of laughing and crying over the sheer beauty of this world, those glimmers of awe that keep pulling me forward.</p>
+
+      <p>This path made me conscious of the patterns still holding me hostage. Of the places I still betrayed myself in the name of love. I thought that if I just loved enough, forgave enough, waited long enough, he would change.</p>
+      
+      <p>He didn’t. But I did.</p>
+
+      <p>What changed was me — and everything around me shifted with it.</p>
+
+      <p>I stopped trying to love him into rising. I started reclaiming my energy, my dharmic purpose, my life. I stopped orbiting him and began anchoring in me. And from that place… the attachment dissolved. The longing eased. The clarity returned.</p>
+
+      <p>I still love him. I always will. But I’m no longer waiting for him to meet me.
+That kind of love, rooted in compassion but detached from outcome is the only thing that can ever truly pull someone forward, if they’re willing to rise.</p>
+
+<p>These days, I’m deeply grateful for the life I’ve built. I’m a mother, a mentor, and a radically unschooling parent. I’ve created a home that honours all our souls. One where healing, truth, and joy get to lead.</p>
+
+<p>I’m also an alternative healthcare practitioner and intuitive mentor, devoted to helping others reclaim their energy, deepen their sovereignty, and come home to themselves. I support those on the Twin Flame path, not just to “get their twin back,” but to remember who they are.</p>
+
+<p>I want to travel more. Keep deepening my connection with my children, and work with people who are ready to stop surviving and start thriving, because that’s what this path is really about.</p>
+
+<span className="font-calligraphy text-[28px] leading-9 p-5">Thank you for being here</span>
+</div>
+    </div>
+  </div>
+  )
 }
--- src/app/(pages)/work-with-me/page.js
+++ src/app/(pages)/work-with-me/page.js
@@ -1,24 +1,68 @@
 import Image from "next/image"
 import connie from "../../../../public/assets/connie.webp"
+import bg from "../../../../public/bg-texture.jpg"
 
 export default function WorkWithMe () {
     return (
-        <div className="flex flex-col justify-center p-10 gap-10 items-center text-center">
+        
+        <div className="flex flex-col h-full justify-center gap-10 items-center text-center inset-0 p-10">
+
+
+<div className="fixed w-full h-full top-0 left-0 -z-10 opacity-50">
+    <Image
+        src={bg}
+        alt=""
+        fill
+        className="object-cover blur-md"
+    />
+</div>
+
+            <div className="flex flex-col py-10 gap-10">
             <h2>WORK WITH ME</h2>
             <p>
-There are a number of ways to engage with this work, depending on where you are on your path and what kind of support you are seeking. All work is offered within a grounded, ethical, and trauma-aware container, with a strong emphasis on self-responsibility, integration, and inner union.
-I aim to support you on the journey back to your true self, helping you heal core wounds, integrate fragmented parts, clear and strengthen your energy system, and move toward greater inner alignment and sovereignty.
-            </p>
+There are a number of ways to engage with this work, depending on where you are on your path and what kind of support you are seeking. All work is offered within a grounded, ethical, and trauma-aware container, with a strong emphasis on self-responsibility, integration, and inner union.</p>
 
-<div className="flex gap-10 flex-col">
-<p className="text-semibold text-shadow-sm"><strong>Spot the difference 👀</strong></p>
-<div className="flex-row flex gap-10">
+<p>
+I aim to support you on the journey back to your true self, helping you heal core wounds, integrate fragmented parts, clear and strengthen your energy system, and move toward greater inner alignment and sovereignty.
+</p>
+            </div>
+    
+<div className="w-full flex gap-10">
+    <div className="flex flex-col p-10 gap-6 w-full justify-evenly">
+
+            <div className="flex flex-col gap-10 p-10 text-center align-middle items-center justify-center rounded-xl border-2 border-mauve-700/50">
+                <h5>one-to-one</h5>
+                <p>Ongoing, relational support for those navigating deep inner work and periods of significant inner change</p>
+
+                <button className="p-3 text-center flex rounded-md hover:bg-mauve-300/10 text-xs bg-mauve-500/20 cursor-pointer">- read more -</button>
+            </div>
+
+            <div className="flex flex-col gap-10 p-10 text-center align-middle items-center justify-center rounded-xl border-2 border-mauve-700/50">
+                <h5>Intuitive Healing / Energetic Clearing</h5>
+                <p>Distance work focused on the energetic layer where many unconscious patterns and relational distortions are held.</p>
+
+                <button className="p-3 text-center flex rounded-md hover:bg-mauve-300/10 text-xs bg-mauve-500/20 cursor-pointer">- read more -</button>
+            </div>
+
+
+            <div className="flex flex-col gap-10 p-10 text-center align-middle items-center justify-center rounded-xl border-2 border-mauve-700/50">
+                <h5>Emotion Code Sessions</h5>
+                <p>Emotion Code work focuses on identifying and releasing trapped emotions using the Emotion Code method, guided through dowsing and intuitive insight.</p>
+
+                <button className="p-3 text-center flex rounded-md hover:bg-mauve-300/10 text-xs bg-mauve-500/20 cursor-pointer">- read more -</button>
+            </div>
+        </div>
+    </div>
+
+<div className="flex gap-10 flex-col w-70 pb-20">
+<div className="gap-10 flex flex-col bg-mauve-800/7 w-full rounded-xl p-10 items-center">
+<p className="text-semibold text-shadow-sm w-35"><strong>Transformations can take on many forms...</strong></p>
 <Image 
     src={connie}
-    alt="bear kemode"
+    alt="connie"
     width={200}
     height={200}
-    className="object-cover rounded-full"
+    className="object-cover rounded-lg"
 />
 
     <Image 
@@ -26,7 +70,7 @@ I aim to support you on the journey back to your true self, helping you heal cor
         alt="bear kemode"
         width={200}
         height={200}
-        className="object-cover rounded-full"
+        className="object-cover rounded-lg"
     />
 </div>
 </div>
